003f17c56437c93b9a0a99c31166c90ab11f5c7c
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-04-22 15:37:29 +0200

    Added tests files

---
+++ liquid-fun/src/test/java/com/github/starnowski/quarkus/fun/liquid/TemplateResourceTest.java
@@ -0,0 +1,42 @@
+package com.github.starnowski.quarkus.fun.liquid;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static io.restassured.RestAssured.given;
+import static java.util.Arrays.asList;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+class TemplateResourceTest {
+
+    private static Stream<Arguments> provideRequestWithTemplateAndExpectedResponse() {
+        return Stream.of(
+                Arguments.of("req1.xml", "xml-to-json-1.liqd", "expected-json-1.json")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideRequestWithTemplateAndExpectedResponse")
+    public void shouldGenerateMapRequestBasedOnTemplate(String requestFile, String templateFile, String expectedContentFile) throws IOException {
+        given()
+                .body(Files.readString(Paths.get(new File(getClass().getClassLoader().getResource(requestFile).getFile()).getPath())))
+                .when().post("/template/{templateId}",templateFile)
+                .then()
+                .statusCode(200)
+                .body(is(Files.readString(Paths.get(new File(getClass().getClassLoader().getResource(expectedContentFile).getFile()).getPath()))));
+    }
+}
