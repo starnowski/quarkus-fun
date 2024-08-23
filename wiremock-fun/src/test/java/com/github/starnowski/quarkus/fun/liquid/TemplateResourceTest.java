4010cf2624baedf7d8a56b5a6840cbccdc7ca577
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-08-23 16:57:29 +0200

    Added test module for wiremock demo

--- wiremock-fun/src/test/java/com/github/starnowski/quarkus/fun/liquid/TemplateResourceTest.java
+++ wiremock-fun/src/test/java/com/github/starnowski/quarkus/fun/liquid/TemplateResourceTest.java
@@ -1,9 +1,14 @@
 package com.github.starnowski.quarkus.fun.liquid;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.json.JSONException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -15,82 +20,60 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.Is.is;
 
 @QuarkusTest
 class TemplateResourceTest {
 
+    private WireMockServer wireMockServer;
+
+    @BeforeEach
+    public void setup(){
+        wireMockServer = new WireMockServer(new WireMockConfiguration().dynamicPort());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        wireMockServer.stop();
+    }
+
     private static Stream<Arguments> provideRequestWithTemplateAndExpectedResponse() {
         return Stream.of(
-                Arguments.of("req1.xml", "xml-to-json-1.liquid", "expected-json-1.json"),
-                Arguments.of("req2.xml", "xml-to-json-1.liquid", "expected-json-1.json"),
-                Arguments.of("req-repeated-elements-with-attributes-and-value.xml", "xml-repeated-elements-with-attributes-and-value-to-json.liquid", "expected-repeated-elements-with-attributes-and-value.json")
-        );
-    }
-
-    private static Stream<Arguments> provideRequestWithTemplateThatHasAttributesAndExpectedResponse() {
-        return Stream.of(
-                Arguments.of("req-with-attributes.xml", "xml-to-json-2.liquid", "expected-json-2.json")
-        );
-    }
-
-    private static Stream<Arguments> provideRequestWithTemplateAndExpectedResponseThatJSONIsGenerallyMatches() {
-        return Stream.of(
-                Arguments.of("req-repeated-elements-with-attributes-and-value.xml", "xml-render-repeated-elements-with-attributes-and-value-to-json.liquid", "expected-render-repeated-elements-with-attributes-and-value.json")
-        );
-    }
-
-    private static Stream<Arguments> provideRequestWithTemplateThatUseCustomFiltersAndExpectedResponse() {
-        return Stream.of(
-                Arguments.of("customFilters/req-without-name-tag.xml", "customFilters/xml-to-json-with-custom-filters-1.liquid", "customFilters/expected-json-with-null-name.json"),
-                Arguments.of("req1.xml", "customFilters/xml-to-json-with-custom-filters-1.liquid", "expected-json-1.json")
+                Arguments.of("req1.xml", "xml-to-json-1.liquid"),
+                Arguments.of("req2.xml", "xml-to-json-1.liquid"),
+                Arguments.of("req-repeated-elements-with-attributes-and-value.xml", "xml-repeated-elements-with-attributes-and-value-to-json.liquid")
         );
     }
 
     @ParameterizedTest
     @MethodSource("provideRequestWithTemplateAndExpectedResponse")
-    public void shouldGenerateMapRequestBasedOnTemplate(String requestFile, String templateFile, String expectedContentFile) throws IOException {
+    public void shouldGenerateMapRequestBasedOnTemplate(String requestFile) throws IOException {
+        // GIVEN
+        wireMockServer.stubFor(get(urlEqualTo("/test")).willReturn(
+                        aResponse()
+                                .withHeader("Content-Type", "application/json")
+                                .withBody("""
+                                { "id": 1, "userId": 1, "title": "my todo" }
+                                """)
+                )
+        );
+
+        String requestBody = Files.readString(Paths.get(new File(getClass().getClassLoader().getResource(requestFile).getFile()).getPath()));
+
+        // WHEN
         given()
-                .body(Files.readString(Paths.get(new File(getClass().getClassLoader().getResource(requestFile).getFile()).getPath())))
-                .when().post("/template/{templateId}", templateFile)
+                .body(requestBody)
+                .when().post("/send")
                 .then()
                 .statusCode(200)
-                .body(is(Files.readString(Paths.get(new File(getClass().getClassLoader().getResource(expectedContentFile).getFile()).getPath()))));
+                .body(is("""
+                                { "id": 1, "userId": 1, "title": "my todo" }
+                                """));
+
+        wireMockServer.verify(1, WireMock.getRequestedFor(WireMock.urlEqualTo("/test")).withRequestBody(WireMock.equalTo(requestBody)));
+
     }
 
-    @ParameterizedTest
-    @MethodSource("provideRequestWithTemplateAndExpectedResponseThatJSONIsGenerallyMatches")
-    public void shouldGenerateMapRequestBasedOnTemplateAndJsonShouldGenerallyMatch(String requestFile, String templateFile, String expectedContentFile) throws IOException, JSONException {
-        ExtractableResponse<Response> response = given()
-                .body(Files.readString(Paths.get(new File(getClass().getClassLoader().getResource(requestFile).getFile()).getPath())))
-                .when().post("/template/{templateId}", templateFile)
-                .then()
-                .statusCode(200).extract();
-
-        org.skyscreamer.jsonassert.JSONAssert.assertEquals(Files.readString(Paths.get(new File(getClass().getClassLoader().getResource(expectedContentFile).getFile()).getPath())), response.body().jsonPath().prettyPrint(), false);
-    }
-
-    @ParameterizedTest
-    @MethodSource({"provideRequestWithTemplateAndExpectedResponse", "provideRequestWithTemplateThatUseCustomFiltersAndExpectedResponse"})
-    public void shouldGenerateMapRequestBasedOnTemplateWithCustomFilters(String requestFile, String templateFile, String expectedContentFile) throws IOException {
-        given()
-                .body(Files.readString(Paths.get(new File(getClass().getClassLoader().getResource(requestFile).getFile()).getPath())))
-                .when().post("/template-with-custom-filters/{templateId}", templateFile)
-                .then()
-                .statusCode(200)
-                .body(is(Files.readString(Paths.get(new File(getClass().getClassLoader().getResource(expectedContentFile).getFile()).getPath()))));
-    }
-
-    @Disabled
-    @ParameterizedTest
-    @MethodSource("provideRequestWithTemplateThatHasAttributesAndExpectedResponse")
-    public void shouldGenerateMapRequestBasedOnTemplateWithAttributes(String requestFile, String templateFile, String expectedContentFile) throws IOException {
-        given()
-                .body(Files.readString(Paths.get(new File(getClass().getClassLoader().getResource(requestFile).getFile()).getPath())))
-                .when().post("/template-with-attributes/{templateId}", templateFile)
-                .then()
-                .statusCode(200)
-                .body(is(Files.readString(Paths.get(new File(getClass().getClassLoader().getResource(expectedContentFile).getFile()).getPath()))));
-    }
 }
